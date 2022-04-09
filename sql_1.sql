415ed1d4ca7f5e7b669c1045edc3158b36fe153d
Anshika Singh <99993107+anshik24@users.noreply.github.com>
2022-04-09 13:34:25 +0530

    Create sql_1.sql

---
+++ sql_1.sql
@@ -0,0 +1,9 @@
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+CITY.jpg
+
+-
+-
+
+select * from CITY where COUNTRYCODE ='JPN';
